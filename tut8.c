172510f7c6fe26dc13e30b77d4d3659ded470213
Manish kasera <66238224+Manish-kasera@users.noreply.github.com>
2021-02-02 21:20:53 +0530

    Add files via upload

---
+++ tut8.c
@@ -0,0 +1,61 @@
+// Operators in C
+
+/* Basic Terms
+
+  1.Operators and Operands..
+   
+   --> An operator is a symbol that tells 
+       the compiler to perform specific mathematical task. 
+  
+   --> operators operates the operands..
+   --> operands or data are same things.
+
+  2.Priority / Precedence..
+
+   -->tells us which operator will do operation first..
+
+  3.Associativity.
+
+   (when priority are same)
+   -->left to right
+   -->right to left                        
+
+ */
+
+#include <stdio.h>
+
+int main()
+{
+  /*  
+    6 + 7
+    +  --->operator
+    6,7 --->operands,data
+  
+  */
+
+  /*  
+     9 + 2 * 3 
+     -->33  (x)(+,*)
+     --> 15 (*,+)
+ */
+
+ /*
+    100 / 10 * 10 
+
+    -->100 (/,*)
+    -->1  (x) (*,/)
+ */
+  return 0;
+}
+
+
+/*
+  Operators in C :
+  1. Arithmetic Operators (+,-,*,/,%,--,++)
+  2. Relational Operators(>,<,>=,<=,!=,==)
+  3. Logical Operators (&& ,||,!)
+  4. Bitwise Operators (&,|,^,etc)
+  5. Assignment Operators (=,+=,-=,etc)
+  6. Misc Operators(sizeof(),?:,etc)
+
+*/
